2bde798a9c6e0315bf748e8a5a4e173d32f2ea64
harrykikkers <harrykikkers@gmail.com>
2026-03-12 14:47:33 +0000

    fix(supabase): secure key and url in local.properties

--- app/src/main/java/com/example/mobile/SupabaseClient.kt
+++ app/src/main/java/com/example/mobile/SupabaseClient.kt
@@ -1,5 +1,6 @@
 package com.example.mobile
 
+import com.example.mobile.BuildConfig
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.auth.auth
@@ -8,6 +9,7 @@ import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.postgrest
 import com.example.mobile.domain.challenges.Challenge
+import io.github.jan.supabase.supabaseJson
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import java.util.UUID
@@ -15,8 +17,8 @@ import java.util.UUID
 object SupabaseClient {
     // Replace with your Supabase project URL and anon key
     private val supabase= createSupabaseClient(
-        supabaseUrl = "https://xzmltjlyybkuuidcaexi.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6bWx0amx5eWJrdXVpZGNhZXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4OTYwODQsImV4cCI6MjA4NjQ3MjA4NH0.pDA8xTfRvsu6LLWo9O6iDlIPdnEqAd-w1YfJVatzgQQ"
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_KEY
     ){
         install(Auth)
         install(Postgrest)
